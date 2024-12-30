cdad0bed2a462ef81a6acd8eb69c69940f561d86
Hewan-Endale <Hewanendale2@gmail.com>
2024-12-30 23:33:52 +0300

    appple react

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from "./Components/Header";
+import MainPart from "./Components/MainPart";
+import Footer from "./Components/Footer";
+function App() {
+  return (
+    <>
+      <Header />
+      <MainPart />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Components/MainPart.jsx
@@ -0,0 +1,23 @@
+import Alert from "./Alert";
+import FIfthSection from "./FIfthSection";
+import FirstSection from "./FirstSection";
+import FourthSection from "./FourthSection";
+import SecondSection from "./SecondSection";
+import SixthSection from "./SixthSection";
+import ThirdSecton from "./ThirdSecton";
+
+function MainPart() {
+  return (
+    <>
+      <Alert />
+      <FirstSection />
+      <SecondSection />
+      <ThirdSecton />
+      <FourthSection />
+      <FIfthSection />
+      <SixthSection />
+    </>
+  );
+}
+
+export default MainPart;
